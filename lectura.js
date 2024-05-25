80469d9ff6b6206dd3cc8174315970c64646ff9d
DaosPath <daospath@gmail.com>
2024-05-25 17:32:41 -0500

    CSol69

--- lectura.js
+++ lectura.js
@@ -5,26 +5,18 @@ window.addEventListener('load', () => {
         const iframe = document.getElementById('lectura-iframe');
         iframe.src = `pdf/${book}`;
     }
+
+    const fullscreenBtn = document.getElementById('fullscreen-btn');
+    fullscreenBtn.addEventListener('click', () => {
+        const iframe = document.getElementById('lectura-iframe');
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) { /* Firefox */
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { /* IE/Edge */
+            iframe.msRequestFullscreen();
+        }
+    });
 });
-
-let currentPage = 1;
-const iframe = document.getElementById('lectura-iframe');
-const prevPageButton = document.getElementById('prev-page');
-const nextPageButton = document.getElementById('next-page');
-
-prevPageButton.addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage--;
-        updatePage();
-    }
-});
-
-nextPageButton.addEventListener('click', () => {
-    currentPage++;
-    updatePage();
-});
-
-function updatePage() {
-    const iframe = document.getElementById('lectura-iframe');
-    iframe.src = `${iframe.src.split('#')[0]}#page=${currentPage}`;
-}
