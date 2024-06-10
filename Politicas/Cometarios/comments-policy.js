aa97e820dcecf575b9886853722b233f9b94ce18
DaosPath <daospath@gmail.com>
2024-06-10 00:39:17 -0500

    Sol1.25

---
+++ Politicas/Cometarios/comments-policy.js
@@ -0,0 +1,59 @@
+document.querySelectorAll('.box').forEach(box => {
+    box.addEventListener('mousemove', (event) => {
+        const boxRect = box.getBoundingClientRect();
+        const mouseX = event.clientX - boxRect.left;
+        const mouseY = event.clientY - boxRect.top;
+        const rotateY = (mouseX / boxRect.width - 0.5) * 5;
+        const rotateX = (mouseY / boxRect.height - 0.5) * -5; 
+        box.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg) scale(1.02)`; 
+    });
+
+    box.addEventListener('mouseleave', () => {
+        box.style.transform = 'rotateY(0deg) rotateX(0deg) scale(1)'; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    function handleResize() {
+        const boxes = document.querySelectorAll('.box');
+        if (window.innerWidth <= 768) {
+            boxes.forEach(box => {
+                box.style.width = '90%';
+                box.style.margin = '10px auto';
+            });
+        } else if (window.innerWidth <= 480) {
+            boxes.forEach(box => {
+                box.style.width = '100%';
+                box.style.margin = '5px auto';
+            });
+        } else {
+            boxes.forEach(box => {
+                box.style.width = '';
+                box.style.margin = '10px';
+            });
+        }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    // Restablece la visibilidad de las cajas cuando se carga la página
+    document.querySelectorAll('.box').forEach(box => {
+        box.classList.remove('hidden');
+        box.style.opacity = '1';
+    });
+
+    // Restablece la opacidad del body al cargar la página
+    document.body.classList.remove('fade-out');
+});
+
+document.querySelectorAll('.link-box').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetUrl = link.getAttribute('href');
+        document.body.classList.add('fade-out');
+        setTimeout(() => {
+            window.location.href = targetUrl;
+        }, 500);
+    });
+});
